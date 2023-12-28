c29e2c2cb7c3879bc488a64fcfbeee6b158680f7
Arash Altafi <arashaltafi1377@gmail.com>
2023-12-28 20:12:46 +0330

    a

--- src/components/OperationComponent.tsx
+++ src/components/OperationComponent.tsx
@@ -1,24 +1,51 @@
+import { useEffect, useRef } from "react"
+
 const OperationComponent = () => {
+
+  const number1 = useRef<HTMLButtonElement | null>(null)
+  const number2 = useRef<HTMLButtonElement | null>(null)
+  const number3 = useRef<HTMLButtonElement | null>(null)
+  const number4 = useRef<HTMLButtonElement | null>(null)
+  const number5 = useRef<HTMLButtonElement | null>(null)
+  const number6 = useRef<HTMLButtonElement | null>(null)
+  const number7 = useRef<HTMLButtonElement | null>(null)
+  const number8 = useRef<HTMLButtonElement | null>(null)
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+      const keyName = event.key
+      console.log(keyName)
+      if (keyName === '1') {
+        number1.current?.click()
+        console.log('clicked')
+      }
+    })
+  }, [])
+
+  const handleClickNumbers = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e)
+  }
+
   return (
-    <div className="w-full px-4 py-2 grid grid-cols-4 grid-rows-5 items-center justify-center gap-4">
+    <div className="w-full h-full px-4 py-2 grid grid-cols-4 grid-rows-5 items-center justify-center gap-4">
       <button className="operation-btn operation-btn-gray">AC</button>
       <button className="operation-btn operation-btn-gray">±</button>
       <button className="operation-btn operation-btn-gray">%</button>
       <button className="operation-btn operation-btn-yellow">÷</button>
 
-      <button className="operation-btn">7</button>
-      <button className="operation-btn">8</button>
+      <button ref={number7} className="operation-btn">7</button>
+      <button ref={number8} className="operation-btn">8</button>
       <button className="operation-btn">9</button>
       <button className="operation-btn operation-btn-yellow">x</button>
 
-      <button className="operation-btn">4</button>
-      <button className="operation-btn">5</button>
-      <button className="operation-btn">6</button>
+      <button ref={number4} onClick={(e) => handleClickNumbers(e)} className="operation-btn">4</button>
+      <button ref={number5} onClick={(e) => handleClickNumbers(e)} className="operation-btn">5</button>
+      <button ref={number6} onClick={(e) => handleClickNumbers(e)} className="operation-btn">6</button>
       <button className="operation-btn operation-btn-yellow">-</button>
 
-      <button className="operation-btn">1</button>
-      <button className="operation-btn">2</button>
-      <button className="operation-btn">3</button>
+      <button ref={number1} onClick={(e) => handleClickNumbers(e)} className="operation-btn">1</button>
+      <button ref={number2} onClick={(e) => handleClickNumbers(e)} className="operation-btn">2</button>
+      <button ref={number3} onClick={(e) => handleClickNumbers(e)} className="operation-btn">3</button>
       <button className="operation-btn operation-btn-yellow">+</button>
 
       <button className="operation-btn col-span-2">0</button>
--- src/components/ResultComponent.tsx
+++ src/components/ResultComponent.tsx
@@ -1,10 +1,10 @@
 const ResultComponent = () => {
   return (
     <div className="w-full px-4 py-2 flex flex-col items-end justify-center gap-2">
-      <p className="text-xl font-normal text-slate-400 dark:text-slate-500">
+      <p className="text-xl font-normal select-text text-slate-400 dark:text-slate-500">
         8,546 * 6,223
       </p>
-      <p className="text-4xl font-bold">
+      <p className="text-4xl font-bold select-all">
         531,817,587
       </p>
     </div>
